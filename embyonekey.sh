f92d6ca94f02c564023b4c0353f596f0bb505c4c
PaBoMe <82071351+LHZ-922@users.noreply.github.com>
2021-09-20 11:31:09 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /etc/nginx/sites-enabled
-wget https://github.com/LHZ-922/embyonekey/proxy.conf
-curl https://github.com/LHZ-922/embyonekey/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
+wget https://raw.githubusercontent.com/LHZ-922/embyonekey/master/proxy.conf
+curl https://raw.githubusercontent.com/LHZ-922/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
 cd /var/packages/EmbyServer/target/mono/bin
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /volume1/web/mb3admin.com
-wget https://github.com/LHZ-922/embyonekey/mb3admin.com.cert.pem
-wget https://github.com/LHZ-922/embyonekey/mb3admin.com.key.pem
+wget https://raw.githubusercontent.com/LHZ-922/embyonekey/master/mb3admin.com.cert.pem
+wget https://raw.githubusercontent.com/LHZ-922/embyonekey/master/mb3admin.com.key.pem
 cat mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://github.com/LHZ-922/embyonekey/user.conf
+wget https://raw.githubusercontent.com/LHZ-922/embyonekey/master/user.conf
 nginx -s reload
 done
