bff184de4051efaa54bd97a1d4348be903a33d15
PaBoMe <82071351+LHZ-922@users.noreply.github.com>
2021-09-20 11:28:02 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /etc/nginx/sites-enabled
 wget https://github.com/LHZ-922/embyonekey/proxy.conf
-curl https://github.com/LHZ-922/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
+curl https://github.com/LHZ-922/embyonekey/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
 cd /var/packages/EmbyServer/target/mono/bin
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /volume1/web/mb3admin.com
