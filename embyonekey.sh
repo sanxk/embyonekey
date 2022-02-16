a80dbfe690ccbeda254a49f7ad79d8233224489e
sanxk <37574632+sanxk@users.noreply.github.com>
2022-02-16 11:25:38 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /etc/nginx/sites-enabled
-wget https://ghproxy.com/https://raw.githubusercontent.com/LHZ-922/embyonekey/master/proxy.conf
-curl https://ghproxy.com/https://raw.githubusercontent.com/LHZ-922/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
+wget https://github.com/sanxk/embyonekey/raw/master/proxy.conf
+curl https://github.com/sanxk/embyonekey/raw/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
 cd /var/packages/EmbyServer/target/mono/bin
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /volume1/web/mb3admin.com
-wget https://ghproxy.com/https://raw.githubusercontent.com/LHZ-922/embyonekey/master/mb3admin.com.cert.pem
-wget https://ghproxy.com/https://raw.githubusercontent.com/LHZ-922/embyonekey/master/mb3admin.com.key.pem
+wget https://github.com/sanxk/embyonekey/raw/master/mb3admin.com.cert.pem
+wget https://github.com/sanxk/embyonekey/raw/master/mb3admin.com.key.pem
 cat mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://ghproxy.com/https://raw.githubusercontent.com/LHZ-922/embyonekey/master/user.conf
+wget https://github.com/sanxk/embyonekey/raw/master/user.conf
 nginx -s reload
 done
